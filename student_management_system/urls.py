e0aa57432968e4b2821d7b9e333ad9932f2aa8da
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-01 06:55:58 -0500

    linked html pages within Django

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login_view"),
+    path("home/", views.home_view, name="home_view"),
+    path("contact/", views.contact_view, name="contact_view"),
+    path("class/", views.class_view, name="class_view"),
+    path("behavior/", views.behavior_view, name="behavior_view"),
+    path("assignments/", views.assignments_view, name="assignments_view"),
 ]
--- student_management_system/views.py
+++ student_management_system/views.py
@@ -8,3 +8,23 @@ from django.template import loader
 def login_view(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def home_view(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
+
+def contact_view(request):
+    template = loader.get_template('contact.html')
+    return HttpResponse(template.render())
+
+def class_view(request):
+    template = loader.get_template('class-info.html')
+    return HttpResponse(template.render())
+
+def assignments_view(request):
+    template = loader.get_template('assignments.html')
+    return HttpResponse(template.render())
+
+def behavior_view(request):
+    template = loader.get_template('behavior.html')
+    return HttpResponse(template.render())
