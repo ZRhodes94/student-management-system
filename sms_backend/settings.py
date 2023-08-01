0324fe86bf58f3b98f7813abf921edf07cffbb4a
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-01 15:40:44 -0500

    updated path

--- sms_backend/settings.py
+++ sms_backend/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/student_management_system/static'),
+    os.path.join(BASE_DIR / 'student_management_system/static'),
 )
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
