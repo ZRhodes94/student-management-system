d72a44fa033207e3ca7ba8f545f7a7130ecc68de
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-01 09:41:14 -0500

    connected to postgreSQL and created tables

--- student_management_system/models.py
+++ student_management_system/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    teacherID = models.IntegerField()
+    name = models.CharField(max_length=80)
+    room = models.IntegerField()
+
+class Student(models.Model):
+    studentID = models.IntegerField()
+    name = models.CharField(max_length=80)
+    gradeLevel = models.IntegerField()
+    parentID = models.IntegerField()
+    courseID = models.IntegerField()
+
+class Parent(models.Model):
+    parentID = models.IntegerField()
+    name = models.CharField(max_length=80)
+    studentID = models.IntegerField()
+
+class Behavior(models.Model):
+    behaviorID = models.IntegerField()
+    description = models.CharField()
+    interventions = models.CharField()
+    studentID = models.IntegerField()
+
+class Course(models.Model):
+    courseID = models.IntegerField()
+    name = models.CharField(max_length=80)
+    description = models.CharField()
+    teacherID = models.IntegerField()
+    assignmentID = models.IntegerField()
+    time = models.IntegerField()
+
+class Assignment(models.Model):
+    assignmentID = models.IntegerField()
+    name = models.CharField(max_length=80)
+    description = models.CharField()
+    pointsPossible = models.IntegerField()
+    pointsEarned = models.IntegerField()
+    studentID = models.IntegerField()
