5c0f284df92cd8640f44b3983eb6f02283cc06a7
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-08 08:22:02 -0500

    updated models

--- student_management_system/models.py
+++ student_management_system/models.py
@@ -2,47 +2,43 @@ from django.db import models
 
 # Create your models here.
 class Teacher(models.Model):
-    teacherID = models.IntegerField()
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     room = models.IntegerField()
 
+class Course(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    time = models.TimeField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=0)
+
 class Student(models.Model):
-    studentID = models.IntegerField()
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     gradeLevel = models.IntegerField()
+    courses = models.ManyToManyField(Course)
 
 class Parent(models.Model):
-    parentID = models.IntegerField()
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
-    studentID = models.IntegerField()
+    students = models.ManyToManyField(Student)
 
 class Behavior(models.Model):
-    behaviorID = models.IntegerField()
-    description = models.CharField()
-    interventions = models.CharField()
-    studentID = models.IntegerField()
-
-class Course(models.Model):
-    courseID = models.IntegerField()
-    name = models.CharField(max_length=80)
-    description = models.CharField()
-    time = models.IntegerField()
-    teacherID = models.IntegerField()
+    id = models.IntegerField(primary_key=True, unique=True)
+    description = models.TextField()
+    interventions = models.TextField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, default=0)
 
 class Assignment(models.Model):
-    assignmentID = models.IntegerField()
+    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
-    description = models.CharField()
+    description = models.TextField()
     pointsPossible = models.IntegerField()
-    courseID = models.IntegerField()
-
-class Student_Courses(models.Model):
-    studentID = models.IntegerField()
-    courseID = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=0)
 
 class Grades(models.Model):
-    assignmentID = models.IntegerField()
-    studentID = models.IntegerField()
-    courseID = models.IntegerField()
+    id = models.IntegerField(primary_key=True, unique=True)
     pointsEarned = models.IntegerField()
-
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, default=0)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE,  default=0)
