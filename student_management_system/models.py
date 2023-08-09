0e515b4d2e525424daa79b367c56d32c7c36b958
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-09 11:28:24 -0500

    course identified as name and time

--- student_management_system/models.py
+++ student_management_system/models.py
@@ -15,7 +15,7 @@ class Course(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=0)
 
     def __str__(self):
-        return self.name
+        return self.name + " " + self.time
 
 class Student(models.Model):
     name = models.CharField(max_length=80)
