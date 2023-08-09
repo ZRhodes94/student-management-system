0ab09ec59b04deecd3a1edb4fb93c85bef6ea49d
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-09 07:57:06 -0500

    admin access to models

--- student_management_system/admin.py
+++ student_management_system/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import Teacher, Student, Parent, Assignment, Grade, Behavior, Course
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Parent)
+admin.site.register(Assignment)
+admin.site.register(Grade)
+admin.site.register(Behavior)
+admin.site.register(Course)
--- student_management_system/models.py
+++ student_management_system/models.py
@@ -2,43 +2,51 @@ from django.db import models
 
 # Create your models here.
 class Teacher(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     room = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     description = models.TextField()
     time = models.TimeField()
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=0)
 
+    def __str__(self):
+        return self.name
+
 class Student(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     gradeLevel = models.IntegerField()
     courses = models.ManyToManyField(Course)
 
+    def __str__(self):
+        return self.name
+
 class Parent(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     students = models.ManyToManyField(Student)
 
+    def __str__(self):
+        return self.name
+
 class Behavior(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
     description = models.TextField()
     interventions = models.TextField()
     student = models.ForeignKey(Student, on_delete=models.CASCADE, default=0)
 
 class Assignment(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=80)
     description = models.TextField()
     pointsPossible = models.IntegerField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE, default=0)
 
-class Grades(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
+    def __str__(self):
+        return self.name
+
+class Grade(models.Model):
     pointsEarned = models.IntegerField()
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, default=0)
     student = models.ForeignKey(Student, on_delete=models.CASCADE,  default=0)
