6dfcdee501394832c5e7f8dfe25021a138940484
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-10 14:19:54 -0500

    removed contact url

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.login_view, name="login"),
     path("home/", views.home_view, name="home_view"),
-    path("contact/", views.contact_view, name="contact_view"),
     path("class/<int:id>", views.class_view, name="class_view"),
     path("behavior/", views.behavior_view, name="behavior_view"),
     path("assignments/", views.assignments_view, name="assignments_view"),
