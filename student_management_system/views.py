06faf1b3329df8757418020cd86b3e4c75910520
Zachary Rhodes <ZRhodes94@gmail.com>
2023-08-10 10:38:00 -0500

    correcting view typo

--- student_management_system/views.py
+++ student_management_system/views.py
@@ -29,8 +29,8 @@ def contact_view(request):
 
 def class_view(request, id):
     teacher = Teacher.objects.get(name='Zachary Rhodes')
-    students = Student.objects.filter(course=id)
-    assignments = Assignment.objects.filter(course=id)
+    students = Student.objects.filter(courses=id)
+    assignments = Assignment.objects.filter(courses=id)
     context = {"teacher": teacher, "assignments": assignments, "students": students}
     context["course"] = Course.objects.get(id = id)
     template = loader.get_template('class-info.html')
